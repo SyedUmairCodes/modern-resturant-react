3a25032d67ff0693d3479277942b78902d786ba4
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-06-27 17:36:17 +0500

    About section WiP

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="section overlay" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon icon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+          beatae provident, perferendis laudantium ducimus vitae voluptatum eius
+          quas dolore facilis voluptatem ipsa animi
+        </p>
+        <button className="custom__button" type="button">
+          Learn More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+    </div>
   </div>
 );
-
 export default AboutUs;
