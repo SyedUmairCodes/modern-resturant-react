3b53318c0fb5021873c3b4884d76c18164e70ea7
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-06-28 14:18:42 +0500

    chef section completed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -4,7 +4,37 @@ import { SubHeading } from "../../components";
 import "./Chef.css";
 
 const Chef = () => (
-  <div className="app__bg app__wrapper section__padding">Chef</div>
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="head chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in </h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="chef's quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            alias velit ratione rerum eveniet provident ad
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea fuga
+          quisquam ut soluta dicta porro distinctio eligendi sint magnam
+          delectus tempore sequi amet cum non, temporibus molestiae{" "}
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Head Chef</p>
+        <img src={images.sign} alt="chef's sign" />
+      </div>
+    </div>
+  </div>
 );
 
 export default Chef;
