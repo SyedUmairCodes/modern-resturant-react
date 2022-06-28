c628249bba217cab06548151d3c3a81c098e48e7
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-06-28 13:11:30 +0500

    Menu section completed

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-
 import "./MenuItem.css";
 
 const MenuItem = ({ title, price, tags }) => (
   <div className="app__menuitem">
-    <div className="app__menuitem-header">
+    <div className="app__menuitem-head">
       {/* Menu items */}
       <div className="app__menuitem-name">
         <p className="p__cormorant" style={{ color: "DCCA87" }}>
