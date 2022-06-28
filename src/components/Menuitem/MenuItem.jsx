846970e0700e092390b6234a7de4f80147755faa
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-06-28 08:40:56 +0500

    menu section WiP

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-header">
+      {/* Menu items */}
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: "DCCA87" }}>
+          {title}
+        </p>
+      </div>
+
+      <div className="app__menuitem-dash" />
+
+      {/* Menu item price */}
+      <div className="app__menuitem-name">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: "#AAAAAA'" }}>
+        {tags}
+      </p>
+    </div>
   </div>
 );
 
