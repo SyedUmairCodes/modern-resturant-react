48abbab6d10a8008085f20bcec42ea862ab986aa
Syed Umair Ali <57952190+SyedUmairCodes@users.noreply.github.com>
2022-07-02 17:32:58 +0500

    Gallery section completed

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -3,9 +3,33 @@ import { images, data } from "../../constants";
 import { SubHeading } from "../../components/";
 import "./Laurels.css";
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
-    Laurels
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards and Recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => (
+          <AwardCard award={award} key={award.title} />
+        ))}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
   </div>
 );
 
